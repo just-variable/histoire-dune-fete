7ff07e8c4d0ca990ec6b7ce0561463076de6bde6
simon <simonhaidar2@icloud.com>
2024-04-07 01:06:16 +0300

    add load animation

---
+++ index.js
@@ -0,0 +1,28 @@
+let allExceptVideo = document.querySelectorAll("*")
+let video = document.querySelectorAll(".video-container *, .video-container")
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+allExceptVideo = Array.from(allExceptVideo);
+allExceptVideo.splice(0, 10);
+console.log(video)
+
+console.log(allExceptVideo)
+
+async function load(){
+    allExceptVideo.forEach(e=>{
+        e.classList.add("hidden")
+    })
+    await sleep(1600);
+    allExceptVideo.forEach(e=>{
+        e.classList.add("show")
+        e.classList.remove("hidden")
+    })
+    video.forEach(e=>{
+        e.classList.add("hidden")
+    })
+}
+
+load();
