f8cfe908f218e18434610edc4963227cc22f0b32
simon <simonhaidar2@icloud.com>
2024-04-07 16:38:02 +0300

    converted css to sass

--- index.js
+++ index.js
@@ -7,21 +7,18 @@ function sleep(ms) {
 
 allExceptVideo = Array.from(allExceptVideo);
 allExceptVideo.splice(0, 10);
-console.log(video)
-
-console.log(allExceptVideo)
 
 async function load(){
     allExceptVideo.forEach(e=>{
-        e.classList.add("hidden")
+        e.classList.add("hidden");
     })
-    await sleep(2100);
+    await sleep(2300);
     allExceptVideo.forEach(e=>{
-        e.classList.add("show")
-        e.classList.remove("hidden")
+        e.classList.add("show");
+        e.classList.remove("hidden");
     })
     video.forEach(e=>{
-        e.classList.add("hidden")
+        e.classList.add("hidden");
     })
 }
 
